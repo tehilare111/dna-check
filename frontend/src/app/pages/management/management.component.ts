e5127dee81e8da0dc00aeb94d7e0ad3797341884
MB <michaelbauer111999@gmail.com>
2020-05-19 05:07:52 +0300

    tree frontend is working.

--- frontend/src/app/pages/management/management.component.ts
+++ frontend/src/app/pages/management/management.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NbIconLibraries } from '@nebular/theme';
+import { TreeComponent, TreeNode } from 'angular-tree-component';
+
+
+/*const actionMapping:IActionMapping = {
+  mouse: {
+    activate: (tree, node, $event) => // Open a modal with node content,
+    click: TREE_ACTIONS.TOGGLE_SELECTED_MULTI,
+  }
+}*/
 
 @Component({
   selector: 'ngx-management',
@@ -7,9 +17,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  constructor() { }
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+  options = {
+    rtl: true,
+    allowDrag: true,
+    allowDrop: true
+  };
+  action=''
+  currentNode=undefined;
+  addUnitInput = '';
+  editUnitInput = '';
+  @ViewChild("tree") private tree: TreeComponent;
+
+  constructor(iconsLibrary: NbIconLibraries) { iconsLibrary.registerFontPack('ion', { iconClassPrefix: 'ion' }); }
 
   ngOnInit(): void {
   }
 
+  onNodePickedUp(event) {
+    this.currentNode = event.node;
+  }
+
+  onNodeDeactivate(event){
+    this.currentNode = undefined;
+    this.action = '';
+  }
+
+  deleteUnit(){
+    let currentNodeParent = this.currentNode.parent;
+    currentNodeParent.data.children = currentNodeParent.data.children
+    .map((el) =>
+      {
+        return el.id==this.currentNode.data.id?undefined:el;
+      }).filter(el => { return el != null } )
+    this.tree.treeModel.update();
+    this.onNodeDeactivate(undefined);
+  }
+
+  addUnit(){
+    // change id
+    if (!this.currentNode.data['children']) { this.currentNode.data['children'] = []; }
+    this.currentNode.data['children'].push({'id':10, 'name':this.addUnitInput, 'children':[]});
+    this.tree.treeModel.update();
+    this.onNodeDeactivate(undefined);
+  }
+
+  editUnit(){
+    this.currentNode.data.name = this.editUnitInput;
+    this.tree.treeModel.update();
+    this.onNodeDeactivate(undefined);
+  }
+
+  getNewId(){
+    return this.nodes
+  }
 }
--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { 
+  NbMenuModule, 
+  NbCardModule, 
+  NbInputModule, 
+  NbIconModule, 
+  NbActionsModule,
+  NbButtonModule,
+} from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
+
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -9,7 +18,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import { ManagementComponent } from './management/management.component';
 //import { ControlTableComponent } from './control-table/control-table.component';
-
+import { TreeModule } from 'angular-tree-component';
 
 @NgModule({
   imports: [
@@ -19,6 +28,13 @@ import { ManagementComponent } from './management/management.component';
     DashboardModule,
     ECommerceModule,
     MiscellaneousModule,
+    TreeModule.forRoot(),
+    NbCardModule,
+    NbIconModule,
+    NbInputModule,
+    NbActionsModule,
+    NbButtonModule,
+    FormsModule
   ],
   declarations: [
     PagesComponent,
